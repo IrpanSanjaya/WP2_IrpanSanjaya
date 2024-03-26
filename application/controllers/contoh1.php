a011e8a3495b68c6c616877e65c3f560341552a6
IrpanSanjaya <irpan.sanjaya195@gmail.com>
2024-03-27 01:54:27 +0700

    menambahkan folder pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Irpan Sanjaya
+            saya tinggal di daerah cakung
+            olahraga yang saya sukai adalah golf";
+    }
+}
